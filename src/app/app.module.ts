7b1906fe93202efc5a8a06500257407ea070ffe2
Ignacio <ignacio.farias.v@usach.cl>
2019-12-13 13:33:05 -0300

    Principal Summary Section
    Section that summarizes the analysis performed at the selected store. It shows information just like the "top k" of most visited stores, in addition to the periods with the highest number of visits between weeks, days and hours.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,30 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LineChartComponent } from './layout/charts/line-chart/line-chart.component';
+import { BarChartComponent } from './layout/charts/bar-chart/bar-chart.component';
+import { TopKComponent } from './layout/top-k/top-k.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    LineChartComponent,
+    BarChartComponent,
+    TopKComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
